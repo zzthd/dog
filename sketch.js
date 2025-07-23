7fdd26d8b51b8c7b41e2aafc94c13adf828d88ef
zzthd <zzthd0928@gmail.com>
2025-07-24 03:05:55 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -94,6 +94,17 @@ function draw() {
     }
   }
   
+  // 在左上角添加一些调试信息
+  fill(0);
+  textSize(16);
+  text(`当前音量: ${vol.toFixed(3)}`, 20, 20);
+  if (isBlowing) {
+    fill(255, 0, 0);
+    text(`正在吹气... 已持续: ${floor(now - blowStartTime)} ms`, 20, 40);
+  }
+}
+
+
 // 🐕 Dog 类
 class Dog {
   constructor(type) {
